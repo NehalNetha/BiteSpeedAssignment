dc3f415b9de06e6d00502982f6f3e61cb8935edd
NehalNetha <nehalnetha249@gmail.com>
2025-05-30 17:07:16 +0530

    check if we're able to extract json from the request

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,21 @@ app.get('/test', (c) => {
   return c.text('This is a test endpoint!');
 });
 
+app.post('/identify', async (c) => {
+  try{
+    const body = await c.req.json<{ email?: string; phoneNumber?: string }>();
+    const { email, phoneNumber } = body;
+
+    if (!email && !phoneNumber) {
+      return c.json({ error: 'Either email or phoneNumber must be provided' }, 400);
+    }
+    
+  } catch (error) {
+    console.error('Error in identify endpoint:', error);
+    return c.json({ error: 'Internal server error' }, 500);
+  }
+});
+
 
 
 export default {
